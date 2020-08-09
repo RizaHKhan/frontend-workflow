43ace7d13db87a20e8f42923da665322765f45fd
khanr <khanriza@gmail.com>
2020-08-09 15:37:15 -0400

    Slider logic

--- src/js/data.js
+++ src/js/data.js
@@ -1,1 +1,9 @@
-let item = [ 1, 2, 3, 4 ]
+const items = [
+  { link: 1, title: 'Prinka Copra', description: 'Former Ms.India', image: 'https://www.kollywoodzone.com/data/media/404/bollywood_bombshell_priyanka_chopra_hot_cleavage_photo_shoot_photos_32.jpg'},
+  { link: 2, title: 'Selma Hayek', description: 'Latina Goddess', image: 'https://i.pinimg.com/originals/45/fc/60/45fc6042b544814759f51ee66b13a389.jpg' },
+  { link: 3, title: 'Marilyn Manroe', description: 'Sex Icon Of the 60s', image: 'https://i.pinimg.com/originals/f8/31/b2/f831b235e8491c80ed4027b28020eb4d.jpg' },
+  { link: 4, title: 'Ashwariya Rai', description: 'Former Ms.India and Ms.Universe', image: 'https://1.bp.blogspot.com/-h0-Kl89TwZM/T_AWLgnb3zI/AAAAAAAAA_Y/Xdcy32UfQ_E/s1600/aishwarya+rai+hot.JPG' },
+  { link: 5, title: 'Jennifer Aniston', description: 'Friends Co-star', image: 'https://img.ti-media.net/wp/uploads/sites/46/2013/08/Jennifer-Aniston-555-614x920.jpg' },
+  { link: 6, title: 'Olivia Munn', description: 'Fashion Model', image: 'https://hips.hearstapps.com/esq.h-cdn.co/assets/15/07/nrm_1423515665-esq-05-munn-xln.jpg?resize=480:*' },
+  { link: 7, title: 'Holy Holms', description: 'Former MMA Champion', image: 'https://i1.wp.com/wmmarankings.com/wp-content/uploads/2020/01/82244004_2638045952917734_8387028774884999168_n.jpg?w=1200' }
+]
