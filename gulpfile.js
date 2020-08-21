946149d3f28c9413bc9ddbd357a7f249ad927c2e
khanr <khanriza@gmail.com>
2020-08-21 13:16:11 -0400

    Added additional libraries for runtime error and deconstructed gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,63 @@
-const gulp = require('gulp')
-const plumber = require('gulp-plumber')
-const del = require('del')
-const concat = require('gulp-concat')
-const babel = require('gulp-babel')
-const sass = require('gulp-sass')
-const browserSync = require('browser-sync').create()
-const cleanCSS = require('gulp-clean-css')
+const { series, parallel, watch, src, dest } = require("gulp");
+const plumber = require("gulp-plumber");
+const del = require("del");
+const concat = require("gulp-concat");
+const babel = require("gulp-babel");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
+const cleanCSS = require("gulp-clean-css");
 
 function html() {
-  return gulp.src('./src/*.html')
-    .pipe(gulp.dest('./dist'))
+  return src("./src/*.html").pipe(dest("./dist"));
 }
 
 function css() {
-  return gulp.src('./src/css/style.scss')
+  return src("./src/css/style.scss")
     .pipe(plumber())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css'))
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("./dist/css"));
 }
 
 function js() {
-  return gulp.src('./src/js/*.js')
+  return src("./src/js/*.js")
     .pipe(plumber())
-    .pipe(babel({
-      presets: ['@babel/env']
-    }))
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('./dist/js'))
+    .pipe(
+      babel({
+        presets: ["@babel/env"],
+        plugins: ["@babel/transform-runtime"],
+      })
+    )
+    .pipe(concat("main.js"))
+    .pipe(dest("./dist/js"));
 }
 
 function clean() {
-  return del(['./dist'])
+  return del(["./dist"]);
 }
 
-function watch() {
+function watchFor() {
   browserSync.init({
     server: {
-      baseDir: './dist/'
-    }
-  })
+      baseDir: "./dist/",
+    },
+  });
 
   // first rerun the function that distributed the css files, then reload the browser
-  gulp.watch('./src/css/**/*.scss').on('change', css)
-  gulp.watch('./dist/css/*.css').on('change', browserSync.reload)
+  watch("./src/css/**/*.scss").on("change", css);
+  watch("./dist/css/*.css").on("change", browserSync.reload);
 
   // first rerun the function that distributed the javascript files, then reload the browser
-  gulp.watch('./src/js/*.js').on('change', js)
-  gulp.watch('./dist/js/*.js').on('change', browserSync.reload)
+  watch("./src/js/*.js").on("change", js);
+  watch("./dist/js/*.js").on("change", browserSync.reload);
+
   // first rerun the function that writes to the dist folder, then reload the browser
-  gulp.watch('./src/*.html').on('change', html)
-  gulp.watch('./dist/*.html').on('change', browserSync.reload)
+  watch("./src/*.html").on("change", html);
+  watch("./dist/*.html").on("change", browserSync.reload);
 }
 
-exports.clean = clean
-exports.css = css
-exports.js = js
-exports.html = html
-exports.watch = watch
-exports.default = gulp.series(clean, html, js, css, watch)
+exports.clean = clean;
+exports.css = css;
+exports.js = js;
+exports.html = html;
+exports.watch = watch;
+exports.default = series(clean, parallel(html, js, css), watchFor);
